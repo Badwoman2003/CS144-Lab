f3b664c48dbf8cbdca83bc39669b7ba948b36e15
Badwoman <3242204470@qq.com>
2024-09-10 23:38:39 +0800

    add unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,14 +5,35 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  uint32_t trans = (n<<32)>>32;
+  uint32_t trans = static_cast<uint32_t>( n & 0xFFFFFFFF );
   return Wrap32 { zero_point + trans };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  Wrap32 check_seq = wrap( checkpoint, zero_point );
+  if ( *this == check_seq ) {
+    return checkpoint;
+  } else {
+    if ( this->raw_value_ < check_seq.raw_value_ ) {
+      uint32_t direct_distance = check_seq.raw_value_ - this->raw_value_;
+      uint32_t wrapped_distance = this->raw_value_ - check_seq.raw_value_;
+
+      if ( direct_distance < wrapped_distance ) {
+        return checkpoint - direct_distance;
+      } else {
+        return checkpoint + wrapped_distance;
+      }
+    } else {
+      uint32_t direct_distance = this->raw_value_ - check_seq.raw_value_;
+      uint32_t wrapped_distance = check_seq.raw_value_ - this->raw_value_;
+
+      if ( direct_distance < wrapped_distance ) {
+        return checkpoint - direct_distance;
+      } else {
+        return checkpoint + wrapped_distance;
+      }
+    }
+  }
 }
