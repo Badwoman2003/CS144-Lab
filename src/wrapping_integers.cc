efca4d3f88bfbb9ff626ba3a6ae3315055776186
Badwoman <3242204470@qq.com>
2024-09-10 09:46:38 +0800

    finished wrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,9 +5,8 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  uint32_t trans = (n<<32)>>32;
+  return Wrap32 { zero_point + trans };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
